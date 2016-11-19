01ee7865513ee9f5489181f642959f8f71361db5
FTwOoO <booopooob@gmail.com>
2016-11-19 14:48:19 +0800

    salsa20实现的bug（原来完全没有加密啊，应该是当时为了测试故意的

--- enc/cipher/salsa20.go
+++ enc/cipher/salsa20.go
@@ -8,23 +8,23 @@ import (
 // Salsa20BlockCrypt implements BlockCrypt
 type Salsa20Stream struct {
 	key [32]byte
+	nonce []byte
 }
 
 // NewSalsa20BlockCrypt initates BlockCrypt by the given key
 func NewSalsa20Stream(key []byte) (StreamCipher, error) {
 	c := new(Salsa20Stream)
 	copy(c.key[:], key)
+	c.nonce = c.key[:8]
 	return c, nil
 }
 
 // Encrypt implements Encrypt interface
 func (c *Salsa20Stream) Encrypt(dst, src []byte) {
-	salsa20.XORKeyStream(dst[:], src[:], c.key[:8], &c.key)
-	copy(dst, src)
+	salsa20.XORKeyStream(dst[:], src[:], c.nonce, &c.key)
 }
 
 // Decrypt implements Decrypt interface
 func (c *Salsa20Stream) Decrypt(dst, src []byte) {
-	salsa20.XORKeyStream(dst[:], src[:], c.key[:8], &c.key)
-	copy(dst, src)
+	salsa20.XORKeyStream(dst[:], src[:], c.nonce, &c.key)
 }
