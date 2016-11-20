0d298e7855871a21a02e4e8c272a6d12acbc7bef
FTwOoO <booopooob@gmail.com>
2016-11-20 16:52:19 +0800

    add file title of GNU listence

--- net/geoip/geoip.go
+++ net/geoip/geoip.go
@@ -1,11 +1,29 @@
-//
-// Modified from https://github.com/oschwald/geoip2-golang/reader.go
-//
-// why not use geoip2-golang directly?
-// 	If you only need several fields, you may get superior performance by using maxminddb's
-// 	Lookup directly with a result struct that only contains the required fields.
-// 	(See example_test.go in the https://github.com/oschwald/maxminddb-golang repository for an example of this.)
-//
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Author: FTwOoO <booobooob@gmail.com>
+ *
+ * Modified from https://github.com/oschwald/geoip2-golang/reader.go
+ *
+ * why not use geoip2-golang directly?
+ * 	If you only need several fields, you may get superior performance by using maxminddb's
+ * 	Lookup directly with a result struct that only contains the required fields.
+ * 	(See example_test.go in the https://github.com/oschwald/maxminddb-golang repository for an example of this.)
+ *
+ */
+
+
 
 package geoip
 
